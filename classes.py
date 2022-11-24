354e7106cb7732ed9b859f21f3bb2d4e7a7b7290
Andrew Zaiko <andrewzaiko@gmail.com>
2022-11-24 19:12:48 +0300

    Add class Area

---
+++ classes.py
@@ -0,0 +1,10 @@
+# from abc import ABC, abstractmethod
+
+
+class Area:  # Make game field (Singleton class)
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls, *args, **kwargs)
+        return cls.__instance
