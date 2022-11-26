a62dd071a51c44fd0f07cdc830f03b98cf00012f
Andrew Zaiko <andrewzaiko@gmail.com>
2022-11-26 14:59:04 +0300

    Привёл класс Area к шаблонам Моносостояние и Синглтон

--- classes.py
+++ classes.py
@@ -1,10 +1,17 @@
 # from abc import ABC, abstractmethod
 
 
-class Area:  # Make game field (Singleton class)
+class Area:
+    """Создаёт игровое поле по шаблонам Синглтон и Моносостояние"""
     __instance = None
+    __shared_fields = {
+
+    }
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls, *args, **kwargs)
         return cls.__instance
+
+    def __init__(self):
+        self.__dict__ = self.__shared_fields
